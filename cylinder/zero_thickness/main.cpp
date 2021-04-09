987c35a8e75aa3caae51baaf6a439aa06fcf3ece
resecmo <resecmo@gmail.com>
2021-04-09 18:57:35 +0300

    Fixed work with boundary problem

--- cylinder/zero_thickness/main.cpp
+++ cylinder/zero_thickness/main.cpp
@@ -54,7 +54,7 @@ mat44 C(vec4 u) {
 }
 
 vec4 phi(vec4 u_prev, vec4 u_cur, vec4 u_next) {
-    vec4 u_part = - u_prev / 12 - u_cur * 5 /6 - u_next / 12;
+    vec4 u_part = u_prev / 12 + u_cur * 5 /6 + u_next / 12; 
     vec4 f_part = tau * (f(u_prev) / 12 + f(u_cur) * 5 / 6 + f(u_next) / 12);
     vec4 jac_part = - tau * (jac(u_prev) * u_prev / 12 + jac(u_cur) * u_cur * 5 / 6 + jac(u_next) * u_next / 12);
     return u_part + f_part + jac_part;
@@ -71,28 +71,33 @@ void step(){
     mat44 p[L];
     vec4 q[L];
 
-    p[0] = eye(4, 4); // why?
-    q[0] = zeros(4, 1); //why?
-    for (int i = 1; i < L; i++) {
+    p[0] = eye(4, 4);
+    q[0] = zeros(4, 1);
+    for (int i = 1; i < L-1; i++) {
         p[i] = - inv(A(conc[i-1]) * p[i-1] - B(conc[i])) * C(conc[i+1]);
-        q[i] = inv(A(conc[i-1]) * p[i-1] - B(conc[i])) * (phi(conc[i-1], conc[i], conc[i+1]) - A(conc[i-1]) * q[i-1]); // think about choosing args for phi
+        q[i] = inv(A(conc[i-1]) * p[i-1] - B(conc[i])) * (phi(conc[i-1], conc[i], conc[i+1]) - A(conc[i-1]) * q[i-1]);
     }
 
-    new_conc[L-1] = q[L-1]; // change
+    // A_last * u[L-2] + B_last * u[L-1] == phi_last
+    mat44 A_last = eye(4, 4) / 6 - 2 * tau * D / (h*h) - tau * jac(conc[L-2]) / 6;
+    mat44 B_last = eye(4, 4) * 5 / 6 + 2 * tau * D / (h*h) - tau * jac(conc[L-1]) * 5 / 6;
+    vec4 phi_last = conc[L-2] / 6 + conc[L-1] * 5 / 6 + f(conc[L-2]) / 6 + f(conc[L-1]) * 5 / 6 - jac(conc[L-2]) * conc[L-2] / 6 - jac(conc[L-1]) * conc[L-1] * 5 / 6;
+    new_conc[L-1] = inv(A_last * p[L-2] + B_last) * (phi_last - A_last * q[L-2]);
     for (int i = L - 2; i > -1; i--){
         new_conc[i] = p[i] * new_conc[i+1] + q[i];
     }
 
+
     std::copy(new_conc, new_conc+L, conc);
-    //return new_conc;
 }
 
 int main() {
-    //std::cout << inv(mat({{3,5,1}, {0,7,0}, {9,6,1}})) << std::endl;
 
     initialize();
     step();    
-   
+    
+    cout << endl << endl;
+
     for (auto &v : conc) {
         std::cout << v << std::endl;
     }
