19b7b8d2640aa539742522ab56bae3d7b68b4ddd
Peter Zakharov <zakharovpnsd@mail.ru>
2021-04-25 18:00:43 +0000

    added fps-cartoon

---
+++ cylinder/discont/kode/const_diff_test/pres/chgaus/4fold/anime.m
@@ -0,0 +1,68 @@
+a = csvread("chnumsol.csv");
+##aa = csvread("zconst_diff_ex.csv");
+xize = size(a)/50
+xize = xize(1)
+z = a(1:50,1:end);
+x = 1:50;
+m = min([0 min(min(z))])
+M = max(max(z))
+n = 50;
+cmap = colormap (viridis (1000));
+##cmap = colormap (summer);
+phi = linspace(-1,1,100)*pi/2;
+circr = 5*cos(phi);
+circz = 26 + 5*sin(phi);
+for i=0:(xize-1)
+j=50*i
+z = a(j+1:j+50, :);
+##zz = aa(i+1, :);
+#z = a(201:300,1:100)*100;
+#z = z - m;
+#z = z / M;
+#z = n * z;
+##figure(i+1);
+##colormap(cmap);
+##plot(x, z(:,1)
+###  , "linestyle", "none", "marker", "x"
+##  , x, zz
+###  , "linestyle", "none", "marker", "x"
+## );
+###ylim([-0.5, 1]);
+##grid on;
+##legend(["numerical";"exact"] ,"fontsize",15);
+##clf;
+##image(z);
+##colorbar
+##filename = sprintf("col%d.png",i)
+##print(i+1,filename);
+clf;
+contourf(z);
+colormap(cmap);
+##caxis([0, max(max(z))+0.001]);
+hold on; plot(circr,circz, 'color', 'red'); hold off;
+##caxis([0 2]);
+##image(z);
+colorbar
+xlabel("R", "fontsize", 15);
+ylabel("Z", "fontsize", 15);
+title(num2str(i*30));
+##saveas(sprintf("ch%d.png",i));
+filename = sprintf("fps/gus%03d.png",i);
+print(1,filename);
+endfor
+
+##a = csvread("uniformity.csv");
+###image(a);
+##contourf(a);
+###caxis([m M]);
+##caxis([2 3]);
+##colormap(cmap);
+##colorbar
+###print -dpng uniformity.png
+
+
+
+
+
+
+
