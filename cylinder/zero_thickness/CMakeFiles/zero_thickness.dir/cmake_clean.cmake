533e6610f2a3c96f676df4f436c08622ff5dcc34
resecmo <resecmo@gmail.com>
2021-05-08 19:11:38 +0300

    included gitignore and cmake files

---
+++ cylinder/zero_thickness/CMakeFiles/zero_thickness.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zero_thickness.dir/main.cpp.o"
+  "zero_thickness"
+  "zero_thickness.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zero_thickness.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
